4e4e92425f2f7a5b6f4739f837105247b09b9fb8
Godfrey Nolan <godfrey@riis.com>
2024-07-20 11:16:28 -0400

    Update app.py
    more than one question

--- app.py
+++ app.py
@@ -3,28 +3,28 @@ import openai
 import config
 import pyttsx3
 
-
 openai.api_key =  config.OPENAI_API_KEY
 
 messages=[
-        {"role": "system", "content": "You are a hotel receptionist who helps people check in."},
+        {"role": "system", "content": "You are a hotel receptionist, \
+                your job is to help customers check in to their room. You can only respond when a customer asks you a question. \ 
+                Do not start the conversation wait for a question."},
     ]
 def transcribe(audio):
     global messages
     media_file = open(audio, "rb")
     transcription = openai.audio.transcriptions.create(
-        model="whisper-1",
-        file=media_file,
+    model="whisper-1",
+    file=media_file,
     )    
-    print(transcription)
+    messages.append({"role": "user", "content": transcription.text})
+    print(messages)
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
-
     AImessage = response.choices[0].message.content
-    print(AImessage)
     engine = pyttsx3.init()
     engine.say(AImessage)
     engine.runAndWait()
@@ -33,16 +33,12 @@ def transcribe(audio):
     for message in messages:
         if message["role"] != 'system':
             chat += message["role"] + ':' + message["content"] + "\n\n"
+    print(chat)
     return chat
 
-
-ui = gr.Interface(
+gr.Interface(
     fn=transcribe,
     live=True,
-    inputs=gr.Audio(sources="microphone", type="filepath"),
+    inputs=gr.Audio(sources="microphone", type="filepath", streaming=True),
     outputs="text",
-)
-
-ui.launch()
-
-
+).launch()
